4b4dbf172ed6a3b835513d32ccbcf1d9d22ef188
ArminBoston <lhao1969@gmail.com>
2014-12-17 00:50:34 -0500

    updated cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function uses a nonsingular matrix as input, and output
+## an inverse matrix. The inputs matrix is chached, so it is faster
+## when it is used agsin.
 
-## Write a short comment describing this function
+## This "makeCacheMatrix" function takes a matrix as input and four
+## methods are defined to manipulate the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m=NULL
+  set<-function(y){
+    x<<-y
+    m=NULL
+  }
+  get<-function() x
+  setInverse<-function(inverseMatrix){
+    m<-inverseMatrix
+  }
+  getInverse<-function() m
+  list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
+## The "CacheSolve" function takes a matrix as input and output a 
+## inverse matrix, which is also cached so that it can be returned 
+## when the function is called again.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m<-x$getInverse()
+  if (!is.null(m)){
+    return(m)
+  }
+  data<-x$get()
+  print(data)
+  m<-solve(data)
+  x$setInverse(m)
+  m
 }
